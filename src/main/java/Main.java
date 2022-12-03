9b1cb69294a06edd732f434a2308d6ca9927c633
Michael Reichardt <michal.reichardt@gmail.com>
2022-12-03 18:51:50 +0100

    Initial commit - Advent of Code 2022 tasks

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 1, Part 1: " + Task2201.getMax());
+        System.out.println("Task 1, Part 2: " + Task2201.getSumOfN(3));
+    }
+
+}
